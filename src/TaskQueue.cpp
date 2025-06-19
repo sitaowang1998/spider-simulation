7fe25a6fa81e78daca63b01221e2060cc0c1f94f
sitaowang1998 <sitaowang1998@outlook.com>
2025-06-19 14:55:36 -0400

    Integrate task fetch into ConcurrentQueue and remove scheduler thread

---
+++ src/TaskQueue.cpp
@@ -0,0 +1,54 @@
+#include "TaskQueue.hpp"
+
+#include <string>
+#include <variant>
+#include <vector>
+
+#include <boost/uuid.hpp>
+#include <spdlog/spdlog.h>
+#include <spider/core/Error.hpp>
+#include <spider/storage/mysql/MySqlStorageFactory.hpp>
+
+namespace simulation {
+
+TaskQueue::TaskQueue(std::string const& storage_url, boost::uuids::uuid const scheduler_id)
+    : m_scheduler_id{scheduler_id} {
+    auto storage_factory = spider::core::MySqlStorageFactory{storage_url};
+    auto conn_result = storage_factory.provide_storage_connection();
+    if (std::holds_alternative<spider::core::StorageErr>(conn_result)) {
+        spdlog::error("Failed to connect to storage: {}",
+                      std::get<spider::core::StorageErr>(conn_result).description);
+        throw std::runtime_error("Failed to connect to storage");
+    }
+    m_conn = std::move(std::get<spider::core::StorageConnection>(conn_result));
+    m_store = storage_factory.provide_metadata_storage();
+}
+
+auto TaskQueue::batch_pop(int count) -> std::vector<spider::core::ScheduleTaskMetadata> {
+    std::lock_guard lock{m_mutex};
+    if (m_task_queue.empty()) {
+        // Fetch tasks from the storage if the queue is empty
+        std::vector<spider::core::ScheduleTaskMetadata> tasks;
+        auto err = m_store->get_ready_tasks(m_conn, m_scheduler_id, &tasks);
+        if (!err.success()) {
+            spdlog::error("Failed to fetch tasks from storage: {}",
+                          err.description);
+            return {};
+        }
+        if (tasks.empty()) {
+            return {};
+        }
+        for (auto& task : tasks) {
+            m_task_queue.push_back(std::move(task));
+        }
+    }
+    std::vector<spider::core::ScheduleTaskMetadata> result;
+    for (size_t i = 0; i < count && !m_task_queue.empty(); ++i) {
+        result.emplace_back(std::move(m_task_queue.front()));
+        m_task_queue.pop_front();
+    }
+    return result;
+}
+
+
+}
---
+++ src/TaskQueue.hpp
@@ -0,0 +1,31 @@
+#ifndef SPIDER_SIMULATION_TASKQUEUE_HPP
+#define SPIDER_SIMULATION_TASKQUEUE_HPP
+
+#include <deque>
+#include <mutex>
+#include <string>
+#include <vector>
+
+#include <boost/uuid.hpp>
+#include <spider/core/Task.hpp>
+#include <spider/storage/MetadataStorage.hpp>
+#include <spider/storage/StorageConnection.hpp>
+
+namespace simulation {
+class TaskQueue {
+public:
+    TaskQueue(std::string const& storage_url, boost::uuids::uuid scheduler_id);
+
+    auto batch_pop(int count) -> std::vector<spider::core::ScheduleTaskMetadata>;
+private:
+    std::unique_ptr<spider::core::MetadataStorage> m_store;
+    spider::core::StorageConnection m_conn;
+
+    boost::uuids::uuid m_scheduler_id;
+
+    std::mutex m_mutex;
+    std::deque<spider::core::ScheduleTaskMetadata> m_task_queue;
+};
+}
+
+#endif
